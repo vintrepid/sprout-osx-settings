399d7634b2d369b13f6379748bf3c7994c672464
Vince Nibler <vintrepid@gmail.com>
2015-10-04 14:33:41 -0700

    oops, still need to define it, just don't remove TimeMachine

--- recipes/timemachine.rb
+++ recipes/timemachine.rb
@@ -6,7 +6,7 @@ end
 
 old_menu_extras = `sudo -u #{node['sprout']['user']} defaults read com.apple.systemuiserver menuExtras`
 # leave TimeMachine in the status bar
-#new_menu_extras = old_menu_extras.split("\n").select { |line| line !~ /TimeMachine.menu/ }.join("\n")
+new_menu_extras = old_menu_extras #.split("\n").select { |line| line !~ /TimeMachine.menu/ }.join("\n")
 
 execute "TimeMachine should NOT appear in the status bar" do
   command "defaults write com.apple.systemuiserver menuExtras \'#{new_menu_extras}\'"
