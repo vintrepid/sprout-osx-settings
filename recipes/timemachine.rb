0c8fe8193fc91a39055555a681e029efb5ebdd31
Vince Nibler <vintrepid@gmail.com>
2015-10-04 13:37:03 -0700

    leave TimeMachine in the status bar

--- recipes/timemachine.rb
+++ recipes/timemachine.rb
@@ -5,7 +5,8 @@ osx_defaults "TimeMachine should NOT ask to use every new disk"   do
 end
 
 old_menu_extras = `sudo -u #{node['sprout']['user']} defaults read com.apple.systemuiserver menuExtras`
-new_menu_extras = old_menu_extras.split("\n").select { |line| line !~ /TimeMachine.menu/ }.join("\n")
+# leave TimeMachine in the status bar
+#new_menu_extras = old_menu_extras.split("\n").select { |line| line !~ /TimeMachine.menu/ }.join("\n")
 
 execute "TimeMachine should NOT appear in the status bar" do
   command "defaults write com.apple.systemuiserver menuExtras \'#{new_menu_extras}\'"
